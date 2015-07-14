68254098130b7ed7ab0c646c59cc7ad0697c4316
Craig Buchanan <craig.buchanan@choa.org>
2015-07-14 14:49:01 -0400

    Revising unit tests to use OrderDictionary

--- Tests/PsIni.Tests.ps1
+++ Tests/PsIni.Tests.ps1
@@ -5,18 +5,21 @@ Import-Module "$ScriptDir\..\PsIni" -Force
 Describe "PsIni" {
 
     # arrange
-    $ini = Join-Path $TestDrive "\Settings.ini"
+    $ini = "$TestDrive\Settings.ini"
 
-    # hash to be persisted and validated
-    $category1=@{"key1"="value1";"key2"="value2"}
-    $category2=@{"key3"="value3";"key4"="value4"}
-
-    $hashIn=@{"category1"=$category1;"category2"=$category2}
+    # values to be persisted
+    $dictIn = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+    $dictIn["Category1"] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+    $dictIn["Category1"]["Key1"] = "Value1"
+    $dictIn["Category1"]["Key2"] = "Value2"
+    $dictIn["Category2"] = New-Object System.Collections.Specialized.OrderedDictionary([System.StringComparer]::OrdinalIgnoreCase)
+    $dictIn["Category2"]["Key3"] = "Value3"
+    $dictIn["Category2"]["Key4"] = "Value4"
 
     Context "Writing INI" {
 
         # act
-        Out-IniFile -inputobject $hashIn -filepath $ini
+        $dictIn | Out-IniFile -FilePath $ini
 
         # assert
         It "creates a file" {
@@ -27,7 +30,7 @@ Describe "PsIni" {
         # assert
         It "content matches expected value" {
 
-            $content = "[category1]`r`nkey1=value1`r`nkey2=value2`r`n[category2]`r`nkey3=value3`r`nkey4=value4`r`n"
+            $content = "[Category1]`r`nKey1=value1`r`nKey2=Value2`r`n`r`n[Category2]`r`nKey3=Value3`r`nKey4=Value4`r`n`r`n"
 
             # http://powershell.org/wp/2013/10/21/why-get-content-aint-yer-friend
             Get-Content $ini | Out-String | Should Be $content
@@ -39,17 +42,17 @@ Describe "PsIni" {
     Context "Reading INI" {
 
         # act
-        Out-IniFile -inputobject $hashIn -filepath $ini
-        $hashOut = Get-IniContent $ini
+        Out-IniFile -inputobject $dictIn -filepath $ini
+        $dictOut = Get-IniContent $ini
 
         # assert
         It "creates a OrderedDictionary from an INI file" {
-            ($hashOut.GetType()) | Should Be System.Collections.Specialized.OrderedDictionary
+            ($dictOut.GetType()) | Should Be System.Collections.Specialized.OrderedDictionary
         }
 
         # assert
         It "content matches original hashtable" {
-            Compare-Object $hashIn $hashOut
+            Compare-Object $dictIn $dictOut
         }
 
     }
