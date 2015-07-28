a0285031a304834ab24c5d7d7cd9452dd799d0b7
Oliver Lipkau <oliver@lipkau.net>
2015-07-28 11:37:00 +0200

    bootstraped layout
    * added affix
    * added Scollspy
    * added Offcanvas

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+//console.log('This would be the main JS file.');
+
+$(document).ready(function () {
+  $('body').scrollspy({ target: '#TableOfContent' });
+  $('[data-toggle="offcanvas"]').click(function () {
+	  console.log('clicked');
+    $('.row-offcanvas').toggleClass('active');
+  });
+  $('nav a').click(function (){
+    $('.row-offcanvas').removeClass('active');
+  });
+});
