a2a0ceabcddef21c79f90bb907d183b7ea7cefc4
seanjseymour <seanjseymour@gmail.com>
2016-08-11 10:44:55 -0700

    Add the new support functions to module

--- PsIni.psm1
+++ PsIni.psm1
@@ -9,6 +9,7 @@
         Author       : Oliver Lipkau <oliver@lipkau.net>
         Contributers : Craig Buchanan <https://github.com/craibuc>
                        Colin Bate <https://github.com/colinbate>
+                       Sean Seymour <https://github.com/seanjseymour>
 
         Homepage     : http://lipkau.github.io/PsIni/
 
@@ -19,3 +20,11 @@ $PsIniModuleHome = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 . "$PsIniModuleHome\Functions\Get-IniContent.ps1"
 
 . "$PsIniModuleHome\Functions\Out-IniFile.ps1"
+
+. "$PsIniModuleHome\Functions\Comment-IniContent.ps1"
+
+. "$PsIniModuleHome\Functions\Uncomment-IniContent.ps1"
+
+. "$PsIniModuleHome\Functions\Remove-IniContent.ps1"
+
+. "$PsIniModuleHome\Functions\Update-IniContent.ps1"
