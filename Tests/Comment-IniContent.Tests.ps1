e6dd8aba3fed14a7ed6378e4ff4ee05cd7139d22
seanjseymour <seanjseymour@gmail.com>
2016-08-11 10:46:34 -0700

    Add tests for the new support functions

---
+++ Tests/Comment-IniContent.Tests.ps1
@@ -0,0 +1,18 @@
+﻿# Enforce WorkingDir
+#--------------------------------------------------
+$Script:ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Path -parent
+Set-Location $ScriptDir
+
+$testFile = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+# functions and tests stored in separate directories; adjusting dot-sourcing
+. "$($ScriptDir -replace 'Tests', 'Functions')\$testFile"
+
+Describe "Comment-IniContent" {
+
+    # assert
+    Context "Alias" {
+        It "Comment-IniContent alias should exist" {
+            Get-Alias -Definition Comment-IniContent | Where-Object {$_.name -eq "comment-ini"} | Measure-Object | Select-Object -ExpandProperty Count | Should Be 1
+        }
+    }
+}
